c980a53c73553ea07ebc8328bf6ab69bbb01a798
Jay <jwinstead@launchdarkly.com>
2018-12-12 14:11:35 -0800

    fix

--- src/components/WebhookIngester.js
+++ src/components/WebhookIngester.js
@@ -13,7 +13,7 @@ module.exports = class WebhookIngester {
     async _handleRequest(req, res) {
         console.debug(`WebhookIngester:${this._operationName}:_handleRequest`)
         let operationData = JSONparseSafe(req.body) || {}
-        
+        console.log({operationData})
         await this._cthulhuInstance.operations.execute(this._operationName, operationData)
         res.send(200)
     }
