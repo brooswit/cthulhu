923d1d31caf2b9dbd1e12d61c5b78495301bda3f
Jay <jwinstead@launchdarkly.com>
2018-12-12 21:35:14 -0800

    fix

--- src/components/SlackIntegration.js
+++ src/components/SlackIntegration.js
@@ -1,9 +1,9 @@
-const WebhookEventIngester = require('./WebhookEventIngester')
+const WebhookIngester = require('./WebhookIngester')
 
 module.exports = class SlackIntegration {
-    constructor(expressApp, token, appName) {
+    constructor(cthulhu, expressApp, token, appName) {
         console.debug(`new SlackIntegration ${appName}`)
-        const webhookEventIngester = new WebhookIngester(
+        const webhookIngester = new WebhookIngester(
             expressApp,  `/injest_slack_event/${appName}`, 
             ({token, challenge, type}) => {
                 console.debug(`SlackIntegration:${appName}:injestSlackEvent(${token}, ${challenge}, ${type})`)
