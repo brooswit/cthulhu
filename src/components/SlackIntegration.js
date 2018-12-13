0e0fa391b067cdcad387758d8e94c5b40f494139
Jay <jwinstead@launchdarkly.com>
2018-12-12 21:18:43 -0800

    fix

--- src/components/SlackIntegration.js
+++ src/components/SlackIntegration.js
@@ -5,7 +5,7 @@ module.exports = class SlackIntegration {
         console.debug(`new SlackIntegration ${appName}`)
         const eventName = `injest_slack_event/${appName}`
         const webhookEventIngester = new WebhookEventIngester(cthulhu, expressApp, eventName)
-        cthulhu.events.on(eventName, this.injestSlackEvent, ({token, challenge, type}) => {
+        cthulhu.events.on(eventName, ({token, challenge, type}) => {
             console.debug(`SlackIntegration:${appName}:injestSlackEvent(${token}, ${challenge}, ${type})`)
             if (challenge) return challenge
             if (token !== token) return false
