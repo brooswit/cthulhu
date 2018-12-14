ea37ddf15c98d07f9173dd57e01a1eef97245dd3
Jay <jwinstead@launchdarkly.com>
2018-12-14 10:29:34 -0800

    fix

--- src/components/ClubhouseIntegration.js
+++ src/components/ClubhouseIntegration.js
@@ -5,10 +5,10 @@ module.exports = class ClubhouseIntegration {
         console.debug(`new ClubhouseIntegration ${appName}`)
         const webhookIngester = new WebhookIngester(
             expressApp,  `/ingest_clubhouse_event/${appName}`, 
-            ({actions}) => {
-                console.log(JSON.stringify(payload))
-                for (actionIndex in actions) {
-                    let action = actions[action]
+            (payload) => {
+                console.log(JSON.stringify())
+                for (actionIndex in payload.actions) {
+                    let action = payload.actions[action]
 
                     cthulhu.events.emit(`clubhouse_event:${appName}`, action)
                     cthulhu.events.emit(`clubhouse_event:${appName}:${action['entity_type']}`, action)
