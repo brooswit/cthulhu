532839904d5d07f7e232837e47f8ddeb0df970fc
Jay <jwinstead@launchdarkly.com>
2018-12-14 09:40:19 -0800

    fix

---
+++ src/components/ClubhouseIntegration.js
@@ -0,0 +1,15 @@
+const WebhookIngester = require('./WebhookIngester')
+
+module.exports = class ClubhouseIntegration {
+    constructor(cthulhu, expressApp, secret, appName) {
+        console.debug(`new ClubhouseIntegration ${appName}`)
+        const webhookIngester = new WebhookIngester(
+            expressApp,  `/ingest_clubhouse_event/${appName}`, 
+            (payload) => {
+                if (payload.secret !== secret) return false
+                cthulhu.events.emit(`clubhouse_event:${appName}`, payload)
+                console.log(payload)
+            }
+        )
+    }
+}
