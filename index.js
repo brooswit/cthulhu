57513b95078bff74f9b2813a44ba5d2af802f561
Jay <jwinstead@launchdarkly.com>
2018-12-14 16:09:28 -0800

    fix

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const WebhookIngester = require('./src/components/WebhookIngester')
 const SlackIntegration = require('./src/components/SlackIntegration')
 const SalesforceIntegration = require('./src/components/SalesforceIntegration')
 const ClubhouseIntegration = require('./src/components/ClubhouseIntegration')
+const ZendeskIntegration = require('./src/components/ZendeskIntegration')
 
 class Cthulhu {
     constructor() {
@@ -18,5 +19,6 @@ Cthulhu.WebhookIngester = WebhookIngester
 Cthulhu.SlackIntegration = SlackIntegration
 Cthulhu.SalesforceIntegration = SalesforceIntegration
 Cthulhu.ClubhouseIntegration = ClubhouseIntegration
+Cthulhu.ZendeskIntegration = ZendeskIntegration
 
 module.exports = Cthulhu
---
+++ src/components/ZendeskIntegration.js
@@ -0,0 +1,27 @@
+const zendeskClientFactory = require('node-zendesk')
+
+module.exports = class ZendeskIntegration {
+    constructor(cthulhu, secret, appName) {
+        console.debug(`new ZendeskIntegration ${appName}`)
+        zendeskClient = zendeskClientFactory.createClient({
+            username:  'jwinstead@launchdarkly.com',
+            token:     'NS422LzY5HilrF0HAagtyyHq7Hdkge6Mkee4IeH4',
+            remoteUri: 'https://launchdarklysupport.zendesk.com/api/v2'
+        })
+        (async() => {
+            while(true) {
+                let orgs = await new Promise((resolve, reject) => {
+                    zendeskClient.organizations.list( (err, req, response) => {
+                        if(err) reject(err)
+                        else resolve(response)
+                    })
+                })
+                console.log('zendesk stuff scraped')
+                for (orgIndex in orgs) {
+                    let org = orgs[orgIndex]
+                    cthulhu.events.emit(`zendesk_event:${appName}:organization:scraped`, org)
+                }
+            }
+        })()
+    }
+}
