aa205b2614e11f54a235cc782f7a54aba03bd0f7
Jay <jwinstead@launchdarkly.com>
2018-12-17 14:31:26 -0800

    fix

--- src/components/ZendeskIntegration.js
+++ src/components/ZendeskIntegration.js
@@ -1,35 +1,11 @@
-const zendeskClientFactory = require('node-zendesk')
+const zendesk = require('node-zendesk')
 module.exports = class ZendeskIntegration {
     constructor(cthulhu, zdUsername, zdToken, appName) {
         console.debug(`new ZendeskIntegration ${appName}`)
-        let zendeskClient = zendeskClientFactory.createClient({
+        let zendeskClient = zendesk.createClient({
             username:  zdUsername,
             token:     zdToken,
             remoteUri: `https://${appName}.zendesk.com/api/v2`
         })
-
-        (async () => {
-            while(true) {
-                let nextCyclePromise = new Promise((resolve, reject) => {
-                    setTimeout(1000 * 60 * 15)
-                    zendeskClient.organizations.list( (err, req, response) => {
-                        if(err) reject(err)
-                        else resolve(response)
-                    })
-                })
-                let orgs = await new Promise((resolve, reject) => {
-                    zendeskClient.organizations.list( (err, req, response) => {
-                        if(err) reject(err)
-                        else resolve(response)
-                    })
-                })
-                console.log('zendesk stuff scraped')
-                for (orgIndex in orgs) {
-                    let org = orgs[orgIndex]
-                    cthulhu.events.emit(`zendesk_event:${appName}:organization:scraped`, org)
-                }
-                await nextCyclePromise
-            }
-        })()
     }
 }
