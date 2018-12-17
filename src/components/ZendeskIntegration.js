966e07596a5d27d701b9b90a875485a574e0a4dc
Jay <jwinstead@launchdarkly.com>
2018-12-17 14:33:15 -0800

    fix

--- src/components/ZendeskIntegration.js
+++ src/components/ZendeskIntegration.js
@@ -7,5 +7,30 @@ module.exports = class ZendeskIntegration {
             token:     zdToken,
             remoteUri: `https://${appName}.zendesk.com/api/v2`
         })
+        
+        this._main()
+    }
+    async _main() {
+        while(true) {
+            let nextCyclePromise = new Promise((resolve, reject) => {
+                setTimeout(1000 * 60 * 15)
+                zendeskClient.organizations.list( (err, req, response) => {
+                    if(err) reject(err)
+                    else resolve(response)
+                })
+            })
+            let orgs = await new Promise((resolve, reject) => {
+                zendeskClient.organizations.list( (err, req, response) => {
+                    if(err) reject(err)
+                    else resolve(response)
+                })
+            })
+            console.log('zendesk stuff scraped')
+            for (orgIndex in orgs) {
+                let org = orgs[orgIndex]
+                cthulhu.events.emit(`zendesk_event:${appName}:organization:scraped`, org)
+            }
+            await nextCyclePromise
+        }
     }
 }
