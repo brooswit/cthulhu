7d739a353884ac9c3bad8e39961fece3579d618c
Jay <jwinstead@launchdarkly.com>
2018-12-19 18:59:42 -0800

    AUTOMATED DEVELOPMENT DEPLOYMENT

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ class Minion {
         await this.untilReady()
         const reqRefId = this._nextReqRefId ++
         this._ws.send(JSON.stringify({ reqRefId, resourceType, action, resourceName, value}))
-        const resRefId = await new Promise((resolve) => {this._responseEvents.once(reqRefId, resolve) })
+        const {ackId, resRefId=value} = await new Promise((resolve) => {this._responseEvents.once(reqRefId, resolve) })
         this._responseEvents.on(reqRefId, function(value) {
             this._ws.send(JSON.stringify({ runRefId, resourceType, action: 'ack'}))
             callback(value)
