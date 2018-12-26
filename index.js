69eef6cd7886eee09c9722f75608631416426846
brooswit <brooswit@OldBoi.local>
2018-12-25 21:17:29 -0800

    AUTOMATED DEVELOPMENT DEPLOYMENT

--- index.js
+++ index.js
@@ -171,17 +171,6 @@ class CthulhuClientHandler {
 
 }
 
-class PromiseToEmit extends Promise {
-    constructor(emitter, eventName, errorEventName) {
-        super((resolve, reject) => {
-            emitter.once(eventName, resolve)
-            if (errorEventName) {
-                emitter.once(errorEventName, reject)
-            }
-        })
-    }
-}
-
 class Minion {
     constructor (url) {
         this.events = new MinionEvents(this)
