9287b758ce0564761cc82d2a42bebe0e43dc5fc2
Jay <jwinstead@launchdarkly.com>
2018-12-25 20:13:22 -0800

    AUTOMATED DEVELOPMENT DEPLOYMENT

--- index.js
+++ index.js
@@ -12,17 +12,31 @@ const SalesforceIntegration = require('./src/components/SalesforceIntegration')
 const ClubhouseIntegration = require('./src/components/ClubhouseIntegration')
 const ZendeskIntegration = require('./src/components/ZendeskIntegration')
 
+
+class EventManager {
+    trigger(eventName, payload) {
+
+    }
+
+    hook(eventName, callback, context) {
+
+    }
+}
+
 class Cthulhu {
     constructor() {
         this._state = Cthulhu.STATE.READY
-        this.internalEvents = new EventEmitter()
-        this.events = new CthulhuEvents()
-        this.tasks = new CthulhuTasks()
+        this._internalEvents = new EventEmitter()
+
+        this._taskManager = new TaskManager()
+        this._eventManager = new EventManager()
 
         this.express = express()
         enableWs(this.express)
         this.express.use(bodyParser.json())
             .ws('/stream', (ws) => { new CthulhuClientHandler(this, ws) })
+
+        this.promiseToStart = new PromiseToEmit(this._internalEvents, 'started')
     }
 
     async start() {
@@ -30,35 +44,41 @@ class Cthulhu {
             this.express.listen(process.env.PORT || 8888, resolve)
         })
     }
-
-    close() {}
-}
-
-class CthulhuEvents {
-    constructor(cthulhu) {
-        this._cthulhu = cthulhu
-
-        this._methodManager = new MethodManager()
+        
+    close() {
+        this._internalEvents.emit('close')
     }
-    async trigger(eventName, value) {
-        return this._methodManager.trigger(eventName, value)
-    }
-    async hook(eventName, callback, context) {
-        return this._methodManager.hook(eventName, callback, context)
-    }
-}
 
-class CthulhuTasks {
-    constructor(cthulhu) {
-        this._cthulhu = cthulhu
+    // Events
+    async triggerEvent(eventName, payload) {
+        await this.promiseToStart
+        this._eventManager.trigger(eventName, payload)
+    }
 
-        this._tasks = new TaskManager()
+    async hookEvent(eventName, eventHandler, context) {
+        await this.promiseToStart
+        this._eventManager.hook(eventName, eventHandler, context)
     }
-    async add(taskName, value) {
-        return this._tasks.add(taskName, value)
+
+    // Tasks
+    async feedTask(taskName, payload) {
+        await this.promiseToStart
+        this._taskManager.feed(taskName, payload)
     }
-    async consume(taskName, callback) {
-        return this._tasks.consumer(taskName, callback)
+
+    async requestTask(taskName, payload, responseHandler, context) {
+        await this.promiseToStart
+        this._taskManager.request(taskName, payload, responseHandler, context)
+    }
+
+    async consumeTask(taskName, taskHandler, context) {
+        await this.promiseToStart
+        this._taskManager.consume(taskName, taskHandler, context)
+    }
+
+    async subscribeTask(taskName, subscriptionHandler, context) {
+        await this.promiseToStart
+        return this._taskManager.subscribe(taskName, subscriptionHandler, context)
     }
 }
 
@@ -71,39 +91,48 @@ class CthulhuClientHandler {
 
         this._ackEmitter = new EventEmitter()
 
-        this._cthulhu.internalEvents.on('close', this._boundClose)
+        this._cthulhu._internalEvents.on('close', this._boundClose)
         this._ws.on('close', this._boundClose)
 
         this._ws.on('message', this._handleMessage.bind(this))
     }
 
-    async _respond (reqRefId, value) {
-        this._ws.send(JSON.stringify({reqRefId, value}))
-    }
-
-    async _request (reqRefId, value) {
-        const ackId = nextAckId++
-        let resolution, rejection
-
-        this._ws.send(JSON.stringify({ackId, reqRefId, value}))
-
-        let result = await new Promise((resolve, reject) => {
-            this._ackEmitter.on(ackId, resolution = resolve)
-            this._ws.on('close', rejection = reject)
-        })
-
-        this._ackEmitter.off(ackId, resolution)
-        this._ws.off('close', rejection)
-
-        return result
-    }
-
     async _handleMessage(str) {
-        const {ackId, reqRefId, resourceType, action, resourceName, value} = JSONparseSafe(str, {})
-        if (action === 'ack') {
-            this._ackEmitter.emit(ackId, value)
+        const { requestId, responseId, methodName, methodCatagory, payload} = JSONparseSafe(str, {})
+        // const {ackId, reqRefId, resourceType, action, resourceName, value} = JSONparseSafe(str, {})
+        if (methodName === 'response') {
+            this._internalEvents.emit(`response:${responseId}`, payload)
         } else {
+            if (methodName === 'triggerEvent') {
+                this._cthulhu.triggerEvent(methodCatagory, payload)
+            } else if (methodName === 'hookEvent') {
+                let hookProcess = await this._cthulhu.hookEvent(eventName, (payload) => {
+                    this._ws.send({requestId, payload})
+                })
+
+                this._ws.on('close', ()=>{
+                    hookProcess.close()
+                })
+            } else if (methodName === 'feedTask') {
+                this._cthulhu.feedTask(methodCatagory, payload)
+            } else if (methodName === 'requestTask') {
+                let requestProcess = this._cthulhu.requestTask(taskName, payload, (payload) => {
+                    this._ws.send({responseId, requestId, payload})
+                })
+
+                this._ws.on('close', ()=>{
+                    requestProcess.close()
+                })
+
+                let responsePayload = await new PromiseToEmit(this._internalEvents, `response:${responseId}`)
+
+            } else if (methodName === 'consumeTask') {
+
+            } else if (methodName === 'subscribeTask') {
+
+            }
             let result = null
+
             if (resourceType === 'events') {
                 if (action === 'trigger') {
                     result = await this._cthulhu.events.trigger(resourceName, value)
@@ -131,60 +160,164 @@ class CthulhuClientHandler {
     }
 
     close() {
-        this._cthulhu.internalEvents.off('close', this._boundClose)        
+        this._cthulhu._internalEvents.off('close', this._boundClose)        
         this._ws.close()
+    }
+
+    async _respond (reqRefId, value) {
+        this._ws.send(JSON.stringify({reqRefId, value}))
+    }
+
+    async _request (reqRefId, value) {
+        const ackId = nextAckId++
+        let resolution, rejection
+
+        this._ws.send(JSON.stringify({ackId, reqRefId, value}))
+
+        let result = await new Promise((resolve, reject) => {
+            this._ackEmitter.on(ackId, resolution = resolve)
+            this._ws.on('close', rejection = reject)
+        })
+
+        this._ackEmitter.off(ackId, resolution)
+        this._ws.off('close', rejection)
+
+        return result
+    }
+
+}
+
+class PromiseToEmit extends Promise {
+    constructor(emitter, eventName, errorEventName) {
+        super((resolve, reject) => {
+            emitter.once(eventName, resolve)
+            if (errorEventName) {
+                emitter.once(errorEventName, reject)
+            }
+        })
     }
 }
 
 class Minion {
     constructor (url) {
-        console.warn('Starting Minion...')
-        this._ws = new WebSocket(`ws://${url}/stream`);
-        this._ws.on('message', this._handleMessage.bind(this))
-        this._nextReqRefId = 0
-        this._responseEvents = new EventEmitter()
-        this._openPromise = new Promise((resolve) => {
-            this._ws.on('open', resolve)
-        })
-
         this.events = new MinionEvents(this)
         this.tasks = new MinionTasks(this)
 
-        this._ws.on('open', ()=>{
-            console.warn('... Minion is ready ...')
-        })
+        this._internalEvents = new EventEmitter()
+        this._nextReqRefId = 0
+
+        this._isStarting = false
+        this._isClosed = false
+
+        this.promiseToStart = new PromiseToEmit(this._internalEvents, 'ready')
+        this.promiseToClose = new PromiseToEmit(this._internalEvents, 'close')
     }
 
-    async _send(resourceType, action, resourceName, value) {
+    start() {
+        if (this._isStarting) return
+        if (this._isClosed) return
+        this._isStarting = true
+        console.warn('Starting Minion...')
+        this._lifecycle()
+    }
+
+    async _lifecycle() {
+        if (this._isClosed) return
+        this._ws = new WebSocket(`ws://${url}/stream`);
+        await new PromiseToEmit(this._ws, 'open')
+        this._ws.on('message', this._handleMessage.bind(this))
+        console.warn('... Minion is ready ...')
+        this._internalEvents.emit('started')
+        await new PromiseToEmit(this._ws, 'close')
+        this.promiseToStart = new PromiseToEmit(this._internalEvents, 'ready')
+        this._lifecycle()
+    }
+
+    close() {
+        if (this._isClosed) return
+        await this.promiseToStart
+
+        this._isClosed = true
+        this._internalEvents.emit('close')
+        this._ws.close()
+    }
+
+    // Events
+    triggerEvent(eventName, value) {
+        this._send('triggerEvent', eventName, value)
+    }
+
+    hookEvent(eventName, callback, context) {
+        return this._subscribe('hookEvent', eventName, callback, context)
+    }
+
+    // Tasks
+    async requestTask(taskName, payload) {
+        return this._fetch('requestTask', taskName, payload)
+    }
+
+    async feedTask(taskName, payload) {
+        this._send('feedTask', taskName, payload)
+    }
+
+    async consumeTask(taskName, taskHandler) {
+        return this._request('consumeTask', taskName, taskHandler)
+    }
+
+    async subscribeTask(taskName, callback, context) {
+        return this._subscribe('subscribeTask', taskName, callback, context)
+    }
+
+    async _fetch(methodName, methodCatagory, payload) {
+        if (this._isClosed) return
+        await this.promiseToStart
+        
+        const requestId = this._nextRequestId ++
+        this._ws.send(JSON.stringify({ requestId, methodName, methodCatagory, payload}))
+        return await new PromiseToEmit(this._internalEvents, `response:${requestId}`)
+    }
+
+    async _send(methodName, methodCatagory, payload) {
+        this._fetch(methodName, methodCatagory, payload)
+    }
+
+    async _request(methodName, methodCatagory, requestHandler, context) {
+        let {responseId, payload} = await this._fetch(methodName, methodCatagory)
+        payload = await requestHandler.call(context, payload)
+        this._send('respond', '', {responseId, payload})
+        return resRefId
+    }
+
+    async _subscribe(methodName, methodCatagory, subscriptionHandler, context) {
+        let {requestId, responseId, payload} = await this._fetch(methodName, methodCatagory)
+        this._internalEvents.on(`response:${requestId}`, subscriptionHandler, context)
+    }
+
+    _ack(ackId, value) {
+        this._ws.send(JSON.stringify({ ackId, action: 'ack', value}))
+    }
 
     async _request(resourceType, action, resourceName, value) {
-        await this.untilReady()
+        await this.promiseToStart()
         const reqRefId = this._nextReqRefId ++
         this._ws.send(JSON.stringify({ reqRefId, resourceType, action, resourceName, value}))
-        return await new Promise((resolve) => { this._responseEvents.once(reqRefId, resolve) })
+        return await new Promise((resolve) => { this._internalEvents.once(reqRefId, resolve) })
     }
 
     async _listen(resourceType, action, resourceName, value, callback) {
-        await this.untilReady()
-        const reqRefId = this._nextReqRefId ++
-        this._ws.send(JSON.stringify({ reqRefId, resourceType, action, resourceName, value}))
-        const {ackId, value} = await new Promise((resolve) => {this._responseEvents.once(reqRefId, resolve) })
-        const resRefId = values
-        this._responseEvents.on(reqRefId, function(value) {
-            this._ws.send(JSON.stringify({ ackId, action: 'ack', value}))
+        await this._request(resourceType, action, resourceName, value) 
+        const resRefId = value
+        this._internalEvents.on(reqRefId, function({ackId, value}) {
+            this._ack(ackId, value)
             callback(value)
         })
         return resRefId
     }
 
-    async untilReady() {
-        await this._openPromise
-    }
-
     _handleMessage(str) {
         const message = JSONparseSafe(str, {})
         const {reqRefId, ackId, value} = message
-        this._responseEvents.emit(reqRefId, {ackId, value})
+        this._internalEvents.emit(reqRefId, {ackId, value})
     }
 
     ready() {}
