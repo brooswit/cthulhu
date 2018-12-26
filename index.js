7414ddf9fa8114faaa385d8eda3e6d491d6c959c
brooswit <brooswit@OldBoi.local>
2018-12-25 22:43:23 -0800

    AUTOMATED DEVELOPMENT DEPLOYMENT

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-const WebhookIngester = 
-const SlackIntegration = require('./src/components/SlackIntegration')
-const SalesforceIntegration = require('./src/components/SalesforceIntegration')
-const ClubhouseIntegration = require('./src/components/ClubhouseIntegration')
-const ZendeskIntegration = require('./src/components/ZendeskIntegration')
-
 Cthulhu = require('./src/Cthulhu')
 Cthulhu.Minion = require('./src/Minion')
-Cthulhu.WebhookIngester = WebhookIngester
-Cthulhu.SlackIntegration = SlackIntegration
-Cthulhu.SalesforceIntegration = SalesforceIntegration
-Cthulhu.ClubhouseIntegration = ClubhouseIntegration
-Cthulhu.ZendeskIntegration = ZendeskIntegration
+Cthulhu.WebhookIngester = require('./src/components/WebhookIngester')
+Cthulhu.SlackIntegration = require('./src/components/SlackIntegration')
+Cthulhu.SalesforceIntegration = require('./src/components/SalesforceIntegration')
+Cthulhu.ClubhouseIntegration = require('./src/components/ClubhouseIntegration')
+Cthulhu.ZendeskIntegration = require('./src/components/ZendeskIntegration')
 
 module.exports = Cthulhu
