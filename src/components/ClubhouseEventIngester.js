96c1aa4eb567d36d83f7ed16dd8e86576e0638ad
brooswit <brooswit@OldBoi.local>
2018-12-28 09:13:30 -0800

    AUTOMATED DEVELOPMENT DEPLOYMENT

---
+++ src/components/ClubhouseEventIngester.js
@@ -0,0 +1,19 @@
+const TaskIngester = require('./TaskIngester')
+
+module.exports = class ClubhouseEventIngester {
+  constructor(cthulhu, clubhouseSecret, clubhouseAppName) {
+    let taskName = `ingest_clubhouse_event/${clubhouseAppName}` // TODO: change to `ingest/clubhouse/${appName}`
+    cthulhu.subscribeTask(taskName, this._injestClubhouseTask)
+    new TaskIngester(cthulhu,  taskName)
+  }
+
+  _injestClubhouseTask(payload) {
+    for (let actionIndex in payload.actions) {
+      let actionPayload = payload.actions[actionIndex]
+      let {entity_type, action} = actionPayload
+      cthulhu.events.emit(`clubhouse/${appName}`, actionPayload)
+      cthulhu.events.emit(`clubhouse/${appName}/${entity_type}`, actionPayload)
+      cthulhu.events.emit(`clubhouse/${appName}/${entity_type}/${action}`, actionPayload)
+    }
+  }
+}
