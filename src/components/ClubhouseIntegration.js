cc2a5756e059f60db5714e74e3fcb103a80c9d8b
Jay <jwinstead@launchdarkly.com>
2018-12-27 17:35:28 -0800

    AUTOMATED DEVELOPMENT DEPLOYMENT

--- src/components/ClubhouseIntegration.js
+++ src/components/ClubhouseIntegration.js
@@ -1,7 +1,7 @@
 const Ingester = require('./TaskIngester')
 module.exports = class ClubhouseEventIngester {
   constructor(cthulhu, secret, appName) {
-    let taskName = `ingest_clubhouse_event/${appName}`
+    let taskName = `ingest_clubhouse_event/${appName}` // TODO: change to `ingest_clubhouse_task/${appName}`
     cthulhu.subscribeTask(taskName, (payload) => {
       for (let actionIndex in payload.actions) {
         let action = payload.actions[actionIndex]
