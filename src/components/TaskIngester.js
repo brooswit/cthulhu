03f15d3a45ba8db849f0f49f9e7faca322bc1b2d
Jay <jwinstead@launchdarkly.com>
2018-12-27 17:13:26 -0800

    AUTOMATED DEVELOPMENT DEPLOYMENT

--- src/components/TaskIngester.js
+++ src/components/TaskIngester.js
@@ -4,11 +4,7 @@ module.exports = class TaskIngester {
     constructor(cthulhu, express, path) {
         let taskName = path.replace('/',':')
         express.post(`/${path}`, async (req, res) => {
-            let operationData = req.body || {};
-            let result = cthulhu.requestTask(taskName)
-            let result = await handler(operationData)
-            if(result === false) res.status(404).end();
-            else res.send(result)
+            cthulhu.requestTask(taskName, req.body || {}, res.send)
         })
     }
 }
