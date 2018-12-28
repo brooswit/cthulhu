c14911620785c1c5b4d354b2dfb9b7d89fa48cfa
Jay <jwinstead@launchdarkly.com>
2018-12-27 17:38:08 -0800

    AUTOMATED DEVELOPMENT DEPLOYMENT

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 Cthulhu = require('./src/Cthulhu')
 Cthulhu.Minion = require('./src/Minion')
 Cthulhu.TaskIngester = require('./src/components/TaskIngester')
-Cthulhu.SlackIntegration = require('./src/components/SlackIntegration')
-Cthulhu.SalesforceIntegration = require('./src/components/SalesforceIntegration')
 Cthulhu.ClubhouseEventIngester = require('./src/components/ClubhouseEventIngester')
-Cthulhu.ZendeskIntegration = require('./src/components/ZendeskIntegration')
+
+// Cthulhu.SlackIntegration = require('./src/components/SlackIntegration')
+// Cthulhu.SalesforceIntegration = require('./src/components/SalesforceIntegration')
+// Cthulhu.ZendeskIntegration = require('./src/components/ZendeskIntegration')
 
 module.exports = Cthulhu
