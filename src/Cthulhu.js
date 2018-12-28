65d18ce14056e4a8468b1ef78e18e5f898c7a9a0
brooswit <brooswit@OldBoi.local>
2018-12-27 23:17:13 -0800

    AUTOMATED DEVELOPMENT DEPLOYMENT

--- src/Cthulhu.js
+++ src/Cthulhu.js
@@ -131,7 +131,7 @@ module.exports = class Cthulhu extends CthulhuHeartß {
     enableWs(this.express)
     this.express.use(bodyParser.json())
       .ws('/stream', (ws) => { new CthulhuClientHandler(this, ws) })
-
+PromiseToEmit
     this.promiseToStart = new PromiseToEmit(this._internalEvents, 'started')
   }
 
