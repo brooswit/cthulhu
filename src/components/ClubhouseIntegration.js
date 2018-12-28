fd629d4828feef59c512ad462e2ccfce85d9ad2c
Jay <jwinstead@launchdarkly.com>
2018-12-27 17:06:35 -0800

    AUTOMATED DEVELOPMENT DEPLOYMENT

--- src/components/ClubhouseIntegration.js
+++ src/components/ClubhouseIntegration.js
@@ -2,7 +2,7 @@ const Ingester = require('./Ingester')
 // TODO: REFACTOR TO NEW PATTERNS
 module.exports = class ClubhouseIngester extends Ingester {
     constructor(cthulhu, secret, appName) {
-        super(cthulhu.express,  `/ingest_clubhouse_event/${appName}`,  (payload) => {
+        super(cthulhu.express,  `ingest_clubhouse_event/${appName}`,  (payload) => {
             for (let actionIndex in payload.actions) {
                 let action = payload.actions[actionIndex]
 
