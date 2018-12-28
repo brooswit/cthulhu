12422e9c11b9d0fcede35fa08a55ff164ca8f162
Jay <jwinstead@launchdarkly.com>
2018-12-27 17:20:54 -0800

    AUTOMATED DEVELOPMENT DEPLOYMENT

--- src/components/ClubhouseIntegration.js
+++ src/components/ClubhouseIntegration.js
@@ -1,16 +1,16 @@
 const Ingester = require('./Ingester')
-// TODO: REFACTOR TO NEW PATTERNS
+// TODO/ REFACTOR TO NEW PATTERNS
 module.exports = class ClubhouseEventIngester extends TaskIngester {
-    constructor(cthulhu, secret, appName) {
-        super(cthulhu,  `ingest_clubhouse_event/${appName}`)
-        cthulhu.subscribeTask(`ingest_clubhouse_event:${appName}`),  (payload) => {
-            for (let actionIndex in payload.actions) {
-                let action = payload.actions[actionIndex]
+  constructor(cthulhu, secret, appName) {
+    super(cthulhu,  `ingest_clubhouse_event/${appName}`)
+    cthulhu.subscribeTask(`ingest_clubhouse_event/${appName}`), (payload) => {
+      for (let actionIndex in payload.actions) {
+        let action = payload.actions[actionIndex]
 
-                cthulhu.events.emit(`clubhouse_event:${appName}`, action)
-                cthulhu.events.emit(`clubhouse_event:${appName}:${action['entity_type']}`, action)
-                cthulhu.events.emit(`clubhouse_event:${appName}:${action['entity_type']}:${action['action']}`, action)
-            }
-        )
-    }
+        cthulhu.events.emit(`clubhouse_event/${appName}`, action)
+        cthulhu.events.emit(`clubhouse_event/${appName}/${action['entity_type']}`, action)
+        cthulhu.events.emit(`clubhouse_event/${appName}/${action['entity_type']}/${action['action']}`, action)
+      }
+    )
+  }
 }
