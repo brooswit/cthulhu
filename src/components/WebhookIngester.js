d928a840c526d05fcf9cd95a54701144144917b6
Jay <jwinstead@launchdarkly.com>
2018-12-27 17:06:49 -0800

    AUTOMATED DEVELOPMENT DEPLOYMENT

--- src/components/WebhookIngester.js
+++ src/components/WebhookIngester.js
@@ -3,7 +3,7 @@ const {JSONparseSafe} = require('brooswit-common')
 module.exports = class WebhookIngester {
     constructor(express, path, handler) {
         console.debug(`new WebhookIngester ${path}`)
-        express.post(path, async (req, res) => {
+        express.post(`/${path}`, async (req, res) => {
             let operationData = req.body || {};
             let result = await handler(operationData)
             if(result === false) res.status(404).end();
