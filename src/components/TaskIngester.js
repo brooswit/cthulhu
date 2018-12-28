e05644e92b0a7d4c6249b7afce7d421081133b98
Jay <jwinstead@launchdarkly.com>
2018-12-27 17:17:01 -0800

    AUTOMATED DEVELOPMENT DEPLOYMENT

--- src/components/TaskIngester.js
+++ src/components/TaskIngester.js
@@ -1,9 +1,9 @@
 const {JSONparseSafe} = require('brooswit-common')
 // TODO: REFACTOR TO NEW PATTERNS
 module.exports = class TaskIngester {
-    constructor(cthulhu, express, path) {
+    constructor(cthulhu, path) {
         let taskName = path.replace('/', ':')
-        express.post(`/${path}`, async (req, res) => {
+        cthulhu.express.post(`/${path}`, async (req, res) => {
             cthulhu.requestTask(taskName, req.body || {}, res.send, res)
         })
     }
