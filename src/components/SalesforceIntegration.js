c1bc55e2eb22bbd1bc6c829f0c494efbed95bb20
Jay <jwinstead@launchdarkly.com>
2019-01-09 13:46:27 -0800

    squash! Automated Development Deployment

--- src/components/SalesforceIntegration.js
+++ src/components/SalesforceIntegration.js
@@ -2,7 +2,6 @@ const WebhookIngester = require('./WebhookIngester')
 // TODO: REFACTOR TO NEW PATTERNS
 module.exports = class SalesforceIntegration {
     constructor(cthulhu, token, accountName) {
-        console.debug(`new SalesforceIntegration ${accountName}`)
         const webhookIngester = new WebhookIngester(
             cthulhu.express,  `/ingest_salesforce_event/${accountName}/enterprise_deal_close`, 
             (payload) => {
