41dd206aff18f6c94097ebf13ceb281604b7bf47
Jay <jwinstead@launchdarkly.com>
2019-01-09 14:17:03 -0800

    squash! Automated Development Deployment

--- src/Minion.js
+++ src/Minion.js
@@ -52,9 +52,9 @@ module.exports = class Minion {
   }
 
   // Events
-  triggerEvent(eventName, value) {
-    return this._send('triggerEvent', eventName, value)
-  }
+  triggerEvent(eventName, payload) {
+    return this._send('triggerEvent', eventName, {payload})
+  }}
 
   hookEvent(eventName, callback, context) {
     return this._subscribe('hookEvent', eventName, callback, context)
