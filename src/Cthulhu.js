ac6d0f7776d01382dfa086604403c6fb4e889e24
Jay <jwinstead@launchdarkly.com>
2019-01-31 19:22:50 -0800

    squash! Automated Development Deployment

--- src/Cthulhu.js
+++ src/Cthulhu.js
@@ -1,5 +1,5 @@
 const {church, Job, EventManager, TaskManager, VirtualWebSocket} = require('brooswit-common')
-
+winston = church('CTHULHU')
 const express = require('express')
 const bodyParser = require('body-parser')
 const enableWs = require('express-ws')
@@ -25,6 +25,7 @@ module.exports = class Cthulhu extends Job {
         useStream=true,  streamPath='/stream'
     }, parentJob) {
         super(async ()=>{
+            church.info('STARTING')
             this._eventManager = new EventManager(this)
             this._taskManager = new TaskManager(this)
             this.untilReady = this.promiseTo('ready')
