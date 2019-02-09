db650a35b56867573bc446e75127f6e0991dd6eb
Jay <jwinstead@launchdarkly.com>
2019-02-08 22:12:06 -0800

    squash! Automated Development Deployment

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const enableWs = require('express-ws')
+(async () => {
+    const express = require('express')
+    const bodyParser = require('body-parser')
+    const enableWs = require('express-ws')
 
-const redis = require('redis')
+    const express = await new Promise((resolve)=>{
+        let expressApp = express()
+        expressApp.use(bodyParser.json())
+        enableWs(expressApp)
+        expressApp.listen(expressPort, () => {
+            resolve(expressApp)
+        })
+    })
+    const redis = require('redis')
 
-const LaunchDarkly = require('ldclient-node');Cthulhu = require('./src/Cthulhu')
-Cthulhu.Minion = require('./src/Minion')
-Cthulhu.TaskIngester = require('./src/components/TaskIngester')
-Cthulhu.ClubhouseEventIngester = require('./src/components/ClubhouseEventIngester')
+    // useRedis=false, redisHost, redisPort, redisPassword,
+    // useLd=false, ldUseRedis=false, ldSdkKey, // ldApiKey,
+    // useExpress=true, expressPort=process.env.PORT,
+    // useStream=true,  streamPath='/stream'
+    // }
+    const LaunchDarkly = require('ldclient-node');Cthulhu = require('./src/Cthulhu')
+    Cthulhu.Minion = require('./src/Minion')
+    Cthulhu.TaskIngester = require('./src/components/TaskIngester')
+    Cthulhu.ClubhouseEventIngester = require('./src/components/ClubhouseEventIngester')
 
-// Cthulhu.SlackIntegration = require('./src/components/SlackIntegration')
-// Cthulhu.SalesforceIntegration = require('./src/components/SalesforceIntegration')
-// Cthulhu.ZendeskIntegration = require('./src/components/ZendeskIntegration')
+    // Cthulhu.SlackIntegration = require('./src/components/SlackIntegration')
+    // Cthulhu.SalesforceIntegration = require('./src/components/SalesforceIntegration')
+    // Cthulhu.ZendeskIntegration = require('./src/components/ZendeskIntegration')
 
-module.exports = Cthulhu
+    module.exports = Cthulhu
+})()
