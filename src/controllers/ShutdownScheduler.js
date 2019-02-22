267e367bb963bad5197498ef3863a0de80d6f4ee
Jay <jwinstead@launchdarkly.com>
2019-02-22 08:33:24 -0800

    squash! Automated Development Deployment

--- src/controllers/ShutdownScheduler.js
+++ src/controllers/ShutdownScheduler.js
@@ -4,7 +4,7 @@ module.exports = class RestartScheduler extends Routine {
     constructor(cthulhu, parent) {
         super(async () => {
             this.log.info('started')
-            await chrono.delay(5 * chrono.minutes)
+            await chrono.delay(1 * chrono.hour)
             process.exit();
         }, parent || cthulhu)
     }
